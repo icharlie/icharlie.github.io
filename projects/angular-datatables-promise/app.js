c2cd21511eb2a6092ba459d27f86fd0241d05a4e
Charlie <icharlie.osu@gmail.com>
2015-03-31 21:13:38 -0700

    prepare angular datatables example

---
+++ projects/angular-datatables-promise/app.js
@@ -0,0 +1,26 @@
+(function(window) {
+    'use strict';
+        angular.module('showCase', ['datatables', 'ngResource'])
+            .controller('DataReloadWithPromiseCtrl', DataReloadWithPromiseCtrl);
+
+        function DataReloadWithPromiseCtrl(DTOptionsBuilder, DTColumnBuilder, $resource, DTInstances) {
+            var vm = this;
+            vm.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
+                return $resource('data.json').query().$promise;
+            }).withPaginationType('full_numbers');
+            vm.dtColumns = [
+                DTColumnBuilder.newColumn('id').withTitle('ID'),
+                DTColumnBuilder.newColumn('firstName').withTitle('First name'),
+                DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
+            ];
+            vm.newPromise = newPromise;
+
+            DTInstances.getLast().then(function (dtInstance) {
+                vm.dtInstance = dtInstance;
+            });
+
+            function newPromise() {
+                return $resource('data.json').query().$promise;
+            }
+        }
+})(window);
