1060392b83d58db7c6b71cf9d31c9c80b3e39062
Charlie <icharlie.osu@gmail.com>
2015-04-04 18:06:41 -0700

    Update create a local promise for angular datatables draft.

--- projects/angular-datatables-promise/app.js
+++ projects/angular-datatables-promise/app.js
@@ -1,26 +1,57 @@
 (function(window) {
     'use strict';
-        angular.module('showCase', ['datatables', 'ngResource'])
-            .controller('DataReloadWithPromiseCtrl', DataReloadWithPromiseCtrl);
+    angular.module('showCase', ['datatables', 'ngResource'])
+        .controller('DataReloadWithPromiseCtrl', DataReloadWithPromiseCtrl);
 
-        function DataReloadWithPromiseCtrl(DTOptionsBuilder, DTColumnBuilder, $resource, DTInstances) {
-            var vm = this;
-            vm.dtOptions = DTOptionsBuilder.fromFnPromise(function() {
-                return $resource('data.json').query().$promise;
-            }).withPaginationType('full_numbers');
-            vm.dtColumns = [
-                DTColumnBuilder.newColumn('id').withTitle('ID'),
-                DTColumnBuilder.newColumn('firstName').withTitle('First name'),
-                DTColumnBuilder.newColumn('lastName').withTitle('Last name').notVisible()
-            ];
-            vm.newPromise = newPromise;
+    function DataReloadWithPromiseCtrl($scope, $q, DTOptionsBuilder, DTColumnBuilder, $resource, DTInstances) {
+        var vm = this;
+        vm.dtOptions = DTOptionsBuilder.fromFnPromise(localPromise)
+            .withPaginationType('full_numbers');
+
+        vm.dtColumns = [
+            DTColumnBuilder.newColumn('id').withTitle('ID').notVisible(),
+            DTColumnBuilder.newColumn('firstName').withTitle('First name'),
+            DTColumnBuilder.newColumn('lastName').withTitle('Last name')
+        ];
+        vm.localPromise = localPromise;
+
+        vm.add = add;
+
+        vm.user = getNewUser();
 
             DTInstances.getLast().then(function (dtInstance) {
-                vm.dtInstance = dtInstance;
-            });
+            vm.dtInstance = dtInstance;
+        });
 
-            function newPromise() {
-                return $resource('data.json').query().$promise;
-            }
+        function getNewUser() {
+            return {
+                id: null,
+                firstName: '',
+                lastName: ''
+            };
         }
+
+        function add() {
+            var newUser = angular.copy(vm.user);
+            // Do ajax call to add a new user to server
+            // Update front-end users model
+            vm.users.push(newUser);
+            vm.user = getNewUser();
+            vm.dtInstance.reloadData();
+        }
+
+        function localPromise() {
+            var dfd = $q.defer();
+            if (vm.users) {
+                dfd.resolve(vm.users);
+            } else {
+                var remotePromise = $resource('data.json').query().$promise;
+                remotePromise.then(function (users) {
+                    vm.users = users;
+                    dfd.resolve(vm.users);
+                });
+            }
+            return dfd.promise;
+        }
+    }
 })(window);
