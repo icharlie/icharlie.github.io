b57a669c6a866f3f34723a11d82a39bd530983d5
Charlie Chang <icharlie.osu@gmail.com>
2017-09-10 16:28:16 -0700

    Remove old blog content.

--- about.md
+++
@@ -1,7 +0,0 @@
----
-layout: page
-title: About
-permalink: /about/
----
-
-I am Charlie Chang and a web developer.
--- assets/js/menu.js
+++
@@ -1,6 +0,0 @@
-(function ($) {
-    $('.logo-readium').click(function(e) {
-        e.preventDefault();
-        $(this).closest('.menu').toggleClass('menu-active');
-    });
-}(jQuery));
